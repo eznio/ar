cc13d77564141d90d350bbea89d13e3b7f22478e
Evgenii Zinder <evgenii.zinder@gmail.com>
2016-07-07 16:28:28 +0300

    Initial commit

---
+++ tests/ArTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace eznio\ar\tests;
+
+use eznio\ar\Ar;
+
+class ArTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider arrayGetDataProvider
+     * @param array $array
+     * @param mixed $path
+     * @param mixed $sampler
+     */
+    public function shouldReturnRightArrayGetValues($array, $path, $sampler)
+    {
+        $result = Ar::get($array, $path);
+
+        $this->assertEquals($sampler, $result);
+    }
+
+    public static function arrayGetDataProvider()
+    {
+        return [
+            [
+                ['a' => 'b', 'c' => 'd', 'e' => 'f'],
+                'c',
+                'd'
+            ],
+            [
+                ['a' => 'b', 'c' => 'd', 'e' => 'f'],
+                'g',
+                null
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                'a.b.c',
+                'd'
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                'a.b',
+                ['c' => 'd']
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                'a',
+                ['b' => ['c' => 'd']]
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                '',
+                null
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                '.b.c',
+                null
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                'a.c',
+                null
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                null,
+                null
+            ],
+            [
+                ['a' => ['b' => ['c' => 'd']]],
+                'totallyWrongPath',
+                null
+            ],
+            [
+                null,
+                'a',
+                null
+            ]
+        ];
+    }
+}
